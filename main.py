4d1b3adae0f62553ad064a77faf6e995f34ac183
pannic <pannic1984@outlook.com>
2023-02-10 13:50:24 +0000

    readme

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import hashlib
+from math import sin
+
 import cv2
 import numpy as np
 
@@ -32,9 +35,12 @@ def resize(image, width=256, height=256):
 
 
 def convert_hash2rgb(hash_list):
+    color_list = []
     for i in range(len(hash_list) // 3):
-        pixel = np.array([hash_list[3 * i], hash_list[3 * i + 1], hash_list[3 * i + 2]])
+        pixel = np.array([hash_list[3 * i], hash_list[3 * i + 1], hash_list[3 * i + 2]], dtype=np.uint8)
         print(pixel)
+        color_list.append(pixel)
+    return color_list
 
 
 # def replace(image, words, hash):
@@ -45,7 +51,7 @@ def convert_char2rgb(char):
     return pixel
 
 
-def replace_pixel(image, words, hash_list):
+def replace_pixel_hide(image, words, hash_list):
     for i in range(len(hash_list) // 2):
         location = [hash_list[2 * i], hash_list[2 * i + 1]]
         image[location[0]][location[1]] = convert_char2rgb(words[i])
@@ -59,16 +65,37 @@ def decimal2qua(hash_list):
     return qua_hash
 
 
+def decimal2bin(hash_list):
+    qua_hash = ""
+    for decimal in hash_list:
+        qua_hash += np.base_repr(decimal, base=2)
+    return qua_hash
+
+
 def adjust_y_position(char, qua_hash, dig_hash):
     # return y position offset for each char and given qua hash
-    if qua_hash == 0:
-        return ord(char) * (dig_hash % 2 * 2 - 1)
-    elif qua_hash == 1:
-        return 2 * ord(char) - dig_hash
-    elif qua_hash == 2:
-        return 2 * ord(char)
-    elif qua_hash == 3:
-        return ((ord(char) - 32) % 8 - 3) + 2 * ord(char)
+    y = ord(char)
+    if qua_hash == "0":
+        y = abs(ord(char) * (dig_hash % 2 * 2 - 1))
+    elif qua_hash == "1":
+        y = 2 * ord(char) - dig_hash
+    elif qua_hash == "2":
+        y = 2 * ord(char)
+    elif qua_hash == "3":
+        y = ((ord(char) - 32) % 8 - 3) + 2 * ord(char)
+
+    else:
+        print(qua_hash)
+    y = 2 * ord(char)
+    if y > 255:
+        return y - 255
+    else:
+        return y
+    # return 2*ord(char)
+
+
+def get_ascii(char):
+    return 2 * ord(char)
 
 
 def hash_hide(path):
@@ -78,7 +105,7 @@ def hash_hide(path):
     hash_array = mh_hash.compute(test)[0]
     decimal2hex(hash_array)
     # convert_hash2rgb(hash_array)
-    image = replace_pixel(test, "no body, not even the rain, has such small hands", hash_array)
+    image = replace_pixel_hide(test, "no body, not even the rain, has such small hands", hash_array)
     cv2.imshow("CRYPT", image)
     cv2.waitKey(0)
 
@@ -91,12 +118,15 @@ def hash_glitch(path):
     m_hash_func = cv2.img_hash.MarrHildrethHash_create()
     m_hash = m_hash_func.compute(test)[0]
     m_hash_hex = decimal2hex(m_hash)
+    m_hash_color = convert_hash2rgb(m_hash)
 
     # used as qua offset
     # 32
     b_hash_func = cv2.img_hash.BlockMeanHash_create()
     b_hash = b_hash_func.compute(test)[0]
     b_hash_qua = decimal2qua(b_hash)
+    b_hash_bin = decimal2bin(b_hash)
+    print(b_hash_qua)
 
     # used as adj digit
     # 40
@@ -110,21 +140,93 @@ def hash_glitch(path):
     p_hash_hex = decimal2hex(p_hash)
     p_hash_ini = p_hash[0]
 
+    image = resize(test)
+
+    poem = "only something in me understands, the voice of your eyes is deeper than all roses"
+
+    count = 0
+    x = hash_string_to_number(poem)
+    space_index = 0
+
+    for char in poem:
+        # qua_index = count % len(b_hash_qua)
+        bin_index = count % len(b_hash_bin)
+        color_index = count % len(m_hash_color)
+        if char != " ":
+            # y = adjust_y_position(char, b_hash_qua[qua_index], r_hash[dig_index])
+            y = get_ascii(char)
+            # print("qua:" + b_hash_qua[qua_index] + "  " + str(y))
+            direction = b_hash_bin[bin_index] == "0"
+            replace_pixel_single(image, x, y, m_hash_color[color_index], direction)
+            x += 2
+        else:
+            x += int(p_hash_hex[space_index % len(p_hash_hex)], 16)
+            space_index += 1
+        if x > 255:
+            x -= 255
+        count += 1
+
+    cv2.imshow("CRYPT", image)
+    cv2.waitKey(0)
+
+
+def replace_pixel_single(image, x, y, target, direction=True):
+    # if y < 128:
+    #     for p in range(y):
+    #         image[x][p] = color
+    # else:
+    #     for p in range(y, 256):
+    #         image[x][p] = color
+    # print(target[0] + "" + origin[0])
+    if direction:
+        origin = image[x][y]
+        for p in range(y):
+            per = p / y
+            color = calculate_color(origin, target, per)
+            image[x][p] = color
+            image[x+1][p] = color
+    else:
+        origin = image[x][255 - y]
+        for p in range(y):
+            per = p / y
+            # print(per)
+            color = calculate_color(origin, target, per)
+            image[x][255 - p] = color
+            image[x+1][255 - p] = color
+
+
+def calculate_color(origin, target, percentage):
+    color = np.array([
+        int(target[0]) + (int(origin[0]) - int(target[0])) * (sin(percentage) / 10 + 0.9),
+        int(target[1]) + (int(origin[1]) - int(target[1])) * (sin(percentage) / 10 + 0.9),
+        int(target[2]) + (int(origin[2]) - int(target[2])) * (sin(percentage) / 10 + 0.9)
+    ], dtype=np.uint8)
+    # print(color)
+    return color
+
+
+def hash_string_to_number(string):
+    """Generate a SHA-256 hash for a string."""
+    str_hash = hashlib.sha1()
+    str_hash.update(string.encode())
+    return int(str_hash.hexdigest()[:2], 16)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # hash_hide("Test8.png")
     test = cv2.imread("Test8.png")
-    av_hash = cv2.img_hash.AverageHash_create()
-    decimal2hex(av_hash.compute(test)[0])
-    bm_hash = cv2.img_hash.BlockMeanHash_create()
-    decimal2hex(bm_hash.compute(test)[0])
-    cm_hash = cv2.img_hash.ColorMomentHash_create()
-    print(cm_hash.compute(test))
-    # mh_hash
-    rv_hash = cv2.img_hash.RadialVarianceHash_create()
-    decimal2hex(rv_hash.compute(test)[0])
+    # av_hash = cv2.img_hash.AverageHash_create()
+    # decimal2hex(av_hash.compute(test)[0])
+    # bm_hash = cv2.img_hash.BlockMeanHash_create()
+    # decimal2hex(bm_hash.compute(test)[0])
+    # cm_hash = cv2.img_hash.ColorMomentHash_create()
+    # print(cm_hash.compute(test))
+    # # mh_hash
+    # rv_hash = cv2.img_hash.RadialVarianceHash_create()
+    # decimal2hex(rv_hash.compute(test)[0])
     # p_hash = cv2.img_hash.PHash_create()
     # decimal2hex(p_hash.compute(test)[0])
+    hash_glitch("Test8.png")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Image Hash Readme
+
+This script contains functions to perform image hash operations using the OpenCV library. The script uses the following hash functions: MarrHildrethHash, BlockMeanHash, and MDC (Mean Difference Central Moments). The main purpose of the script is to perform the following operations:
+
+- Hash an image using the MarrHildrethHash and BlockMeanHash functions.
+- Convert the hash values to hexadecimal and binary representations.
+- Convert the hash values to RGB values for use in image manipulation.
+- Replace pixels in an image with ASCII characters based on the hash values.
+- Glitch an image by adjusting the y-coordinate of pixels based on the hash values.
+
+## How to use the script
+
+The `hash_hide` function takes a path to an image as an argument and performs the following operations:
+
+- Resizes the image to 256x256 pixels.
+- Hashes the image using MarrHildrethHash.
+- Converts the hash values to a hexadecimal representation.
+- Replaces pixels in the image with ASCII characters based on the hash values.
+- Displays the modified image using the OpenCV `imshow` function.
+
+The `hash_glitch` function takes a path to an image as an argument and performs the following operations:
+
+- Resizes the image to 256x256 pixels.
+- Hashes the image using MarrHildrethHash and BlockMeanHash.
+- Converts the hash values to a hexadecimal representation.
+- Converts the hash values to RGB values.
+- Adjusts the y-coordinate of pixels in the image based on the hash values.
+- Displays the modified image using the OpenCV `imshow` function.
+
+## Requirements
+
+The following libraries are required to run the script:
+
+- OpenCV (cv2)
+- Numpy
+
+## Code Organization
+
+The code is organized as follows:
+
+- Import statements for the required libraries.
+- The `print_hi` function is a simple function for testing purposes.
+- The `decimal2hex` function takes a list of decimal values and converts them to a hexadecimal representation.
+- The `resize` function takes an image and resizes it to the specified width and height (default 256x256).
+- The `convert_hash2rgb` function takes a list of decimal values and converts them to RGB values.
+- The `convert_char2rgb` function takes an ASCII character and converts it to an RGB value.
+- The `replace_pixel_hide` function takes an image, a string of ASCII characters, and a list of decimal values and replaces pixels in the image with ASCII characters based on the hash values.
+- The `decimal2qua` function takes a list of decimal values and converts them to a 4-digit base representation.
+- The `decimal2bin` function takes a list of decimal values and converts them to a binary representation.
+- The `adjust_y_position` function takes an ASCII character, a 4-digit base representation, and a binary representation and returns a y-coordinate offset for use in image manipulation.
+- The `get_ascii` function takes an ASCII character and returns its decimal value.
+- The `hash_hide` function takes a path to an image and performs the operations described above to hide ASCII characters in the image.
